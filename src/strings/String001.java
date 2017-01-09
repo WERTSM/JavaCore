933ee0edddd40f0b651eccc151fd9dcb6247e4a4
WERTSM <wertsm.work@gmail.com>
2017-01-09 16:22:16 +0300

    Learning "String pool"

--- src/strings/String001.java
+++ src/strings/String001.java
@@ -9,10 +9,8 @@ public class String001 {
         // строковые литералы в Java это тоже объекты
 
         System.out.println("Hello World!");
-        System.out.println("Строка \"Hello World!\" содержит " + "Hello World!".length()
-                + " символов");
-        System.out.println("Первый символ строки \"Hello World!\" это буква "
-                + "Hello World!".charAt(0));
+        System.out.println("Строка \"Hello World!\" содержит " + "Hello World!".length() + " символов");
+        System.out.println("Первый символ строки \"Hello World!\" это буква " + "Hello World!".charAt(0));
         System.out.println("Hello World!".subSequence(0, 6));
     }
 }
--- src/strings/String002.java
+++ src/strings/String002.java
@@ -3,9 +3,6 @@ package strings;
 /**
  * Created by WERT on 09.01.2017.
  */
-
-import static strings.Sys.iHash;
-
 public class String002 {
 
     private static final int SIZE_OF_STRING = 10_000;
@@ -21,32 +18,32 @@ public class String002 {
         String s5 = new String("Hello"); // Строковый объект
         String s6 = s5;                  // одинаковые ссылки
 
-        System.out.println("хэш объекта s1 = " + iHash(s1) + "  хэш строки " + s1 + " = "
+        System.out.println("хэш объекта s1 = " + System.identityHashCode(s1) + "  хэш строки " + s1 + " = "
                 + s1.hashCode());
-        System.out.println("хэш объекта s2 = " + iHash(s2) + "  хэш строки " + s2 + " = "
+        System.out.println("хэш объекта s2 = " + System.identityHashCode(s2) + "  хэш строки " + s2 + " = "
                 + s2.hashCode());
-        System.out.println("хэш объекта s3 = " + iHash(s3) + "  хэш строки " + s3 + " = "
+        System.out.println("хэш объекта s3 = " + System.identityHashCode(s3) + "  хэш строки " + s3 + " = "
                 + s3.hashCode());
-        System.out.println("хэш объекта s4 = " + iHash(s4) + "  хэш строки " + s4 + " = "
+        System.out.println("хэш объекта s4 = " + System.identityHashCode(s4) + "  хэш строки " + s4 + " = "
                 + s4.hashCode());
-        System.out.println("хэш объекта s5 = " + iHash(s5) + "  хэш строки " + s5 + " = "
+        System.out.println("хэш объекта s5 = " + System.identityHashCode(s5) + "  хэш строки " + s5 + " = "
                 + s5.hashCode());
-        System.out.println("хэш объекта s6 = " + iHash(s6) + "  хэш строки " + s6 + " = "
+        System.out.println("хэш объекта s6 = " + System.identityHashCode(s6) + "  хэш строки " + s6 + " = "
                 + s6.hashCode());
 
         s3 = "world"; // создался новый объект в string pool
         System.out.println("\nстрока s1 = " + s1);  // строка s1 осталась неизмененной
-        System.out.println("хэш объекта s3 = " + iHash(s3) + " строка = " + s3);
+        System.out.println("хэш объекта s3 = " + System.identityHashCode(s3) + " строка = " + s3);
 
         System.out.println();
 
         s6 = "Hello"; // ссылка на существующий объект в string pool
-        System.out.println("хэш объекта s6 = " + iHash(s6) + " строка = " + s6);
+        System.out.println("хэш объекта s6 = " + System.identityHashCode(s6) + " строка = " + s6);
 
         System.out.println();
 
         s4 = s4.intern(); //ищется ссылка на существующий объект в string pool
-        System.out.println("хэш объекта s4 = " + iHash(s4) + " строка = " + s4);
+        System.out.println("хэш объекта s4 = " + System.identityHashCode(s4) + " строка = " + s4);
 
         System.out.println();
 
