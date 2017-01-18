8956cf7139fcaed6bcd071076d276823acc36638
WERT <wert.java@gmail.com>
2017-01-18 06:28:32 +0300

    final and private

---
+++ src/classesAdv/FinalAndPrivat.java
@@ -0,0 +1,21 @@
+package classesAdv;
+
+/**
+ * Created by WERT on 18.01.2017.
+ */
+public class FinalAndPrivat {
+    private void fAP() {
+        System.out.println("Root");
+    }
+
+    public static void main(String[] args) {
+        FinalAndPrivat root = new Branch();
+        root.fAP();
+    }
+}
+
+class Branch extends FinalAndPrivat {
+    void fAP() {
+        System.out.println("Branch");
+    }
+}
